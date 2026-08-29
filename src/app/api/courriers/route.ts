7447400220589431b5c16b567c4e9b608439fbb2
edodrandria22 <randriadode22@gmail.com>
2026-08-29 12:27:05 +0300

    mety numero d'arrive manuelle lors de la creation courrier

--- src/app/api/courriers/route.ts
+++ src/app/api/courriers/route.ts
@@ -6,6 +6,6 @@ export async function GET(request: NextRequest) {
 }
 
 export async function POST(request: NextRequest) {
-  const requiredFields = ["object"];
+  const requiredFields = ["object","numeroArrive"];
   return callApiPost(request, "/courriers", requiredFields, true);
 }
